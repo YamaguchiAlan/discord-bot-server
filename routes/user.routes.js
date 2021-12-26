cea233e48420f6aa51ce78aede49c414d6f8ca8f
YamaguchiAlan <alanyamaguchi72@gmail.com>
2021-12-25 22:16:28 -0300

    some

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,10 @@ const cors = require("cors")
 
 const { getToken, authenticate } = require("../controllers/user.controller")
 
+router.get("/", (req, res) => {
+    res.sendStatus(200)
+})
+
 router.get("/api/token", cors({origin:true, credentials: true}), getToken)
 
 router.get("/api/authenticate", authenticate)
