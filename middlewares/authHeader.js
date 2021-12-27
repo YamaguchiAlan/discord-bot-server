eb1ec0b21e131fcc782f06f6f9a90e6f7669e156
YamaguchiAlan <alanyamaguchi72@gmail.com>
2021-12-27 17:59:35 -0300

    auth-header

--- middlewares/authHeader.js
+++ middlewares/authHeader.js
@@ -3,7 +3,11 @@ const secret = process.env.HEADER_SECRET;
 const authHeader = (req, res, next) => {
     const auth = req.headers["origin-auth-secret"];
 
-    if(auth === secret){
+    const validOrigins = ["https://yamabot-bot.tk", "https://www.yamabot-bot.tk", "https://main.d37sq3gn5zbb0w.amplifyapp.com"];
+    const origin = req.headers["origin"];
+    const isOrigin = validOrigins.includes(origin)
+
+    if(auth === secret && isOrigin){
         return next();
     }
 
