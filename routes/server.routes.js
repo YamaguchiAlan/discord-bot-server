65a3c3a7266dd2379ea31be519215daefaf1ee78
YamaguchiAlan <alanyamaguchi72@gmail.com>
2021-12-27 15:17:52 -0300

    auth-header

--- routes/server.routes.js
+++ routes/server.routes.js
@@ -4,22 +4,20 @@ const authHeader = require("../middlewares/authHeader")
 const {checkServer, getServers, getNewNotification, postNotification,
     getNotifications, getNotification, editNotification, removeNotification} = require("../controllers/server.controller")
 
-router.use(authHeader)
+router.get("/api/servers", authHeader, getServers)
 
-router.get("/api/servers", getServers)
+router.get("/api/users/:userId/guilds/:guildId/check", authHeader, checkServer)
 
-router.get("/api/users/:userId/guilds/:guildId/check", checkServer)
+router.get("/api/servers/:guildId/notifications/@new", authHeader, getNewNotification)
 
-router.get("/api/servers/:guildId/notifications/@new", getNewNotification)
+router.post("/api/servers/:guildId/notifications", authHeader, postNotification)
 
-router.post("/api/servers/:guildId/notifications", postNotification)
+router.get("/api/servers/:guildId/notifications", authHeader, getNotifications)
 
-router.get("/api/servers/:guildId/notifications", getNotifications)
+router.get("/api/servers/:guildId/notifications/:notificationId", authHeader, getNotification)
 
-router.get("/api/servers/:guildId/notifications/:notificationId", getNotification)
+router.put("/api/servers/:guildId/notifications/:notificationId", authHeader, editNotification)
 
-router.put("/api/servers/:guildId/notifications/:notificationId", editNotification)
-
-router.delete("/api/servers/:guildId/notifications/:notificationId", removeNotification)
+router.delete("/api/servers/:guildId/notifications/:notificationId", authHeader, removeNotification)
 
 module.exports = router
